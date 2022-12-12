bc3235049d14de996b508b698d7d7cff12f043d6
Eugen Oswald <oswaldeugen95@gmail.com>
2022-12-12 20:39:38 +0100

    startpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StartpageComponent } from './startpage/startpage.component';
+import { WorkTogetherComponent } from './work-together/work-together.component';
+import { SkillsSetComponent } from './skills-set/skills-set.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StartpageComponent,
+    WorkTogetherComponent,
+    SkillsSetComponent,
+    MyWorkComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/work-together/work-together.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-together',
+  templateUrl: './work-together.component.html',
+  styleUrls: ['./work-together.component.scss']
+})
+export class WorkTogetherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
