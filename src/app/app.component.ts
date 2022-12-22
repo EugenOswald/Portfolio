85a60ce684b4afe3a1ce4161a76a80aa7bdd35d8
Eugen Oswald <oswaldeugen95@gmail.com>
2022-12-22 14:40:18 +0100

    update resp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,5 @@ export class AppComponent {
   title = 'portfolio';
 
   constructor(public translate: TranslateService) {
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    // translate.use('de');
   }
 }
